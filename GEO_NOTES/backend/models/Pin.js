0cbd147ec3a0ef6d7c24b63b8e9cc9a1eb180eee
Duc Tran <quytrongduct@student.unimelb.edu.au>
2025-09-20 23:24:43 +1000

    modified Pin.js

--- GEO_NOTES/backend/models/Pin.js
+++ GEO_NOTES/backend/models/Pin.js
@@ -21,35 +21,3 @@ PinSchema.index({ latitude: 1, longitude: 1 });
 
 export default mongoose.model("Pin", PinSchema);
 
-// import mongoose from 'mongoose';
-
-// const PinSchema = new mongoose.Schema({
-//   latitude: {
-//     type: Number,
-//     required: true
-//   },
-//   longitude: {
-//     type: Number,
-//     required: true
-//   },
-//   note: {
-//     type: String,
-//     default: ''
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   },
-//   updatedAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// // Update the updatedAt field before saving
-// PinSchema.pre('save', function(next) {
-//   this.updatedAt = Date.now();
-//   next();
-// });
-
-// export default mongoose.model('Pin', PinSchema);
